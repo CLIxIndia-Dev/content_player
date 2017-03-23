b6248c936164d661fb7bb0f54e961dc4f5347d7b
cjshaw <cjshaw@mit.edu>
2017-03-23 11:02:27 -0400

    add middleware to refresh unplatform session

---
+++ client/js/middleware/unplatform_session_management.js
@@ -0,0 +1,24 @@
+import api         from "../libs/api";
+import Network     from '../constants/network';
+import { DONE }    from "../constants/wrapper";
+import { browserHistory } from 'react-router';
+import { ANALYTICS_ACTION_MAP } from './analytics_logging';
+
+
+const SessionExpiration = store => next => action => {
+
+  if(ANALYTICS_ACTION_MAP[action.type]) {
+    const state = store.getState();
+    // call up to the parent window (unplatform)
+    // and trigger it's startSessionCounter()
+    console.log('calling parent window', state.settings.loggingApiUrl);
+
+    window.parent.postMessage('startSessionCounter',
+      state.settings.loggingApiUrl);
+  }
+
+  // call the next middleWare
+  next(action);
+};
+
+export { SessionExpiration as default };
